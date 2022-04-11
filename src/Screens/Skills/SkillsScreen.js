0be99cd0f1aa54e7cb930b0f2d650360e389e439
ranaivosoa-rindra <91080678+ranaivosoa-rindra@users.noreply.github.com>
2022-04-11 13:40:33 +0300

    middle SKills screen

--- src/Screens/Skills/SkillsScreen.js
+++ src/Screens/Skills/SkillsScreen.js
@@ -1,11 +1,84 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
+import React, { useEffect, useState } from "react";
 import GlobalStyles from "../../Styles/GlobalStyles";
+import SkillsStyles from "./SkillsStyles";
+import BackgroundImage from "../../Components/BackgroundImage";
+import StatusBarComp from "../../Components/StatusBarComp";
+import FooterIcons from "../../Components/FooterIcons";
+import { Dimensions } from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 const SkillsScreen = ({ navigation }) => {
+  const [isLandscape, setIsLandscape] = useState(false);
+
+  const changeLandscape = () => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    setIsLandscape(true);
+  };
+
+  const changePortrait = () => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+    setIsLandscape(false);
+  };
+
+  if (isLandscape) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity onPress={() => changePortrait()}>
+          {/* <Image
+            source={require("../../Images/pics/skillsChart.png")}
+            style={{
+              width: Dimensions.get("window").height / 1.75,
+              height: Dimensions.get("window").width / 1.75,
+              borderRadius: 10,
+              resizeMode: "contain",
+            }}
+          /> */}
+          <Image
+            source={require("../../Images/pics/skillsChart.png")}
+            style={{
+              width: Dimensions.get("screen").width,
+              height: Dimensions.get("screen").height,
+              borderRadius: 10,
+              resizeMode: "cover",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
-    <View style={GlobalStyles.container}>
-      <Text>SkillsScreen</Text>
+    <View style={SkillsStyles.container}>
+      {/* <Text>SkillsScreen</Text> */}
+      <BackgroundImage />
+      <View style={SkillsStyles.body}>
+        <View style={SkillsStyles.chartView}>
+          <TouchableOpacity
+            onPress={() => {
+              changeLandscape();
+            }}
+          >
+            <Image
+              source={require("../../Images/pics/skillsChart.png")}
+              style={{
+                width: 350,
+                height: 207,
+                borderRadius: 10,
+                resizeMode: "contain",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={SkillsStyles.toolsView}></View>
+      </View>
+      <FooterIcons />
+      <StatusBarComp hidden={true} />
     </View>
   );
 };
---
+++ src/Screens/Skills/SkillsStyles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const SkillsStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  body: {
+    width: "100%",
+    height: "80%",
+    // backgroundColor: "#fff",
+  },
+  chartView: {
+    height: "50%",
+    // backgroundColor: "red",
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  toolsView: {
+    height: "50%",
+    // backgroundColor: "yellow",
+    position: "relative",
+  },
+});
+
+export default SkillsStyles;
