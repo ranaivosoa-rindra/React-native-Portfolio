a06f8c9725e9322b07ff53dd2be19b1b0e71577f
ranaivosoa-rindra <91080678+ranaivosoa-rindra@users.noreply.github.com>
2022-04-11 18:16:19 +0300

    Skills Screen v1

--- src/Screens/Skills/SkillsScreen.js
+++ src/Screens/Skills/SkillsScreen.js
@@ -1,83 +1,128 @@
 import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
-import React, { useEffect, useState } from "react";
-import GlobalStyles from "../../Styles/GlobalStyles";
+import React, { useRef } from "react";
 import SkillsStyles from "./SkillsStyles";
 import BackgroundImage from "../../Components/BackgroundImage";
 import StatusBarComp from "../../Components/StatusBarComp";
 import FooterIcons from "../../Components/FooterIcons";
-import { Dimensions } from "react-native";
-import * as ScreenOrientation from "expo-screen-orientation";
+import { Icon } from "react-native-elements/dist/icons/Icon";
+import * as Animatable from "react-native-animatable";
 
 const SkillsScreen = ({ navigation }) => {
-  const [isLandscape, setIsLandscape] = useState(false);
-
-  const changeLandscape = () => {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-    setIsLandscape(true);
+  const openPhoto = () => {
+    Linking.openURL("https://i.ibb.co/ydJxgj3/skills-Chart.png");
   };
 
-  const changePortrait = () => {
-    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
-    setIsLandscape(false);
+  const openGithub = () => {
+    Linking.openURL("https://github.com/");
   };
 
-  if (isLandscape) {
+  const openXD = () => {
+    Linking.openURL("https://www.adobe.com/");
+  };
+
+  const openPs = () => {
+    Linking.openURL("https://www.adobe.com/fr/products/photoshop.html");
+  };
+
+  function chartFunction() {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <TouchableOpacity onPress={() => changePortrait()}>
-          {/* <Image
-            source={require("../../Images/pics/skillsChart.png")}
-            style={{
-              width: Dimensions.get("window").height / 1.75,
-              height: Dimensions.get("window").width / 1.75,
-              borderRadius: 10,
-              resizeMode: "contain",
-            }}
-          /> */}
+      <View style={SkillsStyles.chartView}>
+        <TouchableOpacity
+          onPress={() => {
+            openPhoto();
+          }}
+        >
           <Image
             source={require("../../Images/pics/skillsChart.png")}
-            style={{
-              width: Dimensions.get("screen").width,
-              height: Dimensions.get("screen").height,
-              borderRadius: 10,
-              resizeMode: "cover",
-            }}
+            style={SkillsStyles.chartImage}
           />
         </TouchableOpacity>
       </View>
     );
   }
-  return (
-    <View style={SkillsStyles.container}>
-      {/* <Text>SkillsScreen</Text> */}
-      <BackgroundImage />
-      <View style={SkillsStyles.body}>
-        <View style={SkillsStyles.chartView}>
+
+  const anim = useRef();
+
+  function toolsFunction() {
+    return (
+      <View style={SkillsStyles.toolsView}>
+        <Animatable.View style={SkillsStyles.iconTextView} ref={anim}>
           <TouchableOpacity
+            style={{ flexDirection: "row", alignItems: "center" }}
             onPress={() => {
-              changeLandscape();
+              if (true) {
+                anim.current.flipInX(800);
+              }
             }}
           >
-            <Image
-              source={require("../../Images/pics/skillsChart.png")}
+            <Text style={SkillsStyles.toolText}>Tools</Text>
+            <Icon
+              name="gear"
+              type="font-awesome"
+              color="#000"
+              size={23}
               style={{
-                width: 350,
-                height: 207,
-                borderRadius: 10,
-                resizeMode: "contain",
+                paddingLeft: 6,
               }}
             />
           </TouchableOpacity>
+        </Animatable.View>
+        <View style={SkillsStyles.toolsThreeIconsView}>
+          {githubFunction()}
+          {adobeXDFunction()}
+          {adobePsFunction()}
         </View>
-        <View style={SkillsStyles.toolsView}></View>
       </View>
-      <FooterIcons />
+    );
+
+    function adobePsFunction() {
+      return (
+        <TouchableOpacity onPress={() => openPs()}>
+          <View style={SkillsStyles.singleIconView}>
+            <Image
+              source={require("../../Images/icons/adobe-photoshop.png")}
+              style={[SkillsStyles.iconImage, { width: 46, height: 46 }]}
+            />
+          </View>
+        </TouchableOpacity>
+      );
+    }
+
+    function adobeXDFunction() {
+      return (
+        <TouchableOpacity onPress={() => openXD()}>
+          <View style={SkillsStyles.singleIconView}>
+            <Image
+              source={require("../../Images/icons/xd.png")}
+              style={SkillsStyles.iconImage}
+            />
+          </View>
+        </TouchableOpacity>
+      );
+    }
+
+    function githubFunction() {
+      return (
+        <TouchableOpacity onPress={() => openGithub()}>
+          <View style={SkillsStyles.singleIconView}>
+            <Image
+              source={require("../../Images/icons/github.png")}
+              style={SkillsStyles.iconImage}
+            />
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  }
+
+  return (
+    <View style={SkillsStyles.container}>
+      <BackgroundImage />
+      <View style={SkillsStyles.body}>
+        {chartFunction()}
+        {toolsFunction()}
+      </View>
+      <FooterIcons navigation={navigation} nextScreen="ProjectsScreen" />
       <StatusBarComp hidden={true} />
     </View>
   );
--- src/Screens/Skills/SkillsStyles.js
+++ src/Screens/Skills/SkillsStyles.js
@@ -15,11 +15,53 @@ const SkillsStyles = StyleSheet.create({
     position: "relative",
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 20,
+  },
+  chartImage: {
+    width: 365,
+    height: 222,
+    borderRadius: 10,
+    resizeMode: "contain",
   },
   toolsView: {
     height: "50%",
     // backgroundColor: "yellow",
     position: "relative",
+  },
+  iconTextView: {
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#78D4E2",
+    alignItems: "center",
+    paddingVertical: 7,
+    marginHorizontal: 135,
+    borderRadius: 10,
+  },
+  toolText: {
+    fontSize: 23,
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+  toolsThreeIconsView: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    // marginTop: 50,
+    height: "100%",
+    // backgroundColor: "red",
+    marginTop: 50,
+    alignItems: "flex-start",
+  },
+  singleIconView: {
+    width: 80,
+    height: 80,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
   },
 });
 
